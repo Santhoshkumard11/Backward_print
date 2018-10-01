156586c6bf80d22ac9e6f68aef9fb4da8741d09d
Santhoshkumard11 <31656855+Santhoshkumard11@users.noreply.github.com>
2018-10-01 21:14:21 +0530

    Add files via upload

---
+++ backward.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+int main()
+{
+	char str[100]="santhosh";char srev[100],tem[100];	
+	//std :: cin>>str;
+	int l=strlen(str),c;
+	for(c=0;c<l,l!=0;c++,l--)
+	{
+		tem[c]=str[c];
+		srev[l]=str[c];
+	}
+	l=strlen(str);
+	for(int i=1;i<=l;i++)
+	{
+
+	    std::cout<<srev[i];
+	}
+	return(0);
+}
